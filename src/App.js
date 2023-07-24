2f6157b382416f7321ae2d3571c1835ecc17e59f
DevMedic11 <122469097+DevMedic11@users.noreply.github.com>
2023-07-24 14:54:33 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+import Result from "./Result";
+import Summary from "./Summary";
+export default function App() {
+  return (
+    <div className="App">
+      <Result />
+      <Summary />
+      <button>Continue</button>
+    </div>
+  );
+}
---
+++ src/Result.js
@@ -0,0 +1,16 @@
+export default function Result() {
+  return (
+    <>
+      <h3>Your Result</h3>
+      <div>
+        <h2>
+          <span>76</span> of 100
+        </h2>
+      </div>
+      <h1>Great</h1>
+      <p>
+        You scored higher than 65% of the people who have taken these tests.
+      </p>
+    </>
+  );
+}
